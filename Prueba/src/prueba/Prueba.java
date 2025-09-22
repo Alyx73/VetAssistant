a879b129bab397b6be9b6a159e06987caf96ecb6
alex <alex.pinero.rumbo@gmail.com>
2025-09-22 13:31:38 +0200

    Update Prueba.java

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -14,7 +14,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bye world");
+        System.out.println("Bye-bye world");
     }
     
 }
